010f1d4ffbd3fa6a6be1f34f1f264fab1bd8d64f
Giovanni Izzi <giovanni9972@libero.it>
2021-04-01 13:01:19 +0200

    Add switch + fix card button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pricing-angular';
+  checkbox: any;
+
+  ngOnInit() {
+    this.checkbox = document.querySelector('#checkbox');
+    setInterval(function() {
+      if(!this.checkbox.checked) {
+        document.getElementById('price').innerHTML = "$199.99";
+        document.getElementById('price2').innerHTML = "$249.99";
+        document.getElementById('price3').innerHTML = "$399.99";
+      }
+      else {
+        document.getElementById('price').innerHTML = "$19.99";
+        document.getElementById('price2').innerHTML = "$24.99";
+        document.getElementById('price3').innerHTML = "$39.99";
+      }
+    }
+
+    )
+  }
 }
