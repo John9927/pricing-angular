6590b42acd70ccfc414de80b26353e890b7b34b7
Giovanni Izzi <giovanni9972@libero.it>
2021-04-01 16:05:58 +0200

    Fix mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCBqrAWnANHyB3P1t6uZ6jua6i4x1mawTE",
-    authDomain: "switch-angular.firebaseapp.com",
-    projectId: "switch-angular",
-    storageBucket: "switch-angular.appspot.com",
-    messagingSenderId: "606224008164",
-    appId: "1:606224008164:web:bb1cbdcb0034f4af103df1",
-    measurementId: "G-ERTD75C88S"
+
   }
 };
